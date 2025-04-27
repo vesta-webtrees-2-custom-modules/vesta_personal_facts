79c2d151b6503ba4ce83a83b270489ed4bd4ec5d
Richard Cissée <ric@richard-cissee.de>
2025-04-27 10:15:46 +0200

    Fix #160.

--- patchedWebtrees/Module/IndividualFactsTabModule_2x.php
+++ patchedWebtrees/Module/IndividualFactsTabModule_2x.php
@@ -72,7 +72,7 @@ class IndividualFactsTabModule_2x extends IndividualFactsTabModule implements Mo
             ->flatten();
 
         // Don't show family meta-data tags
-        $exclude_facts  = new Collection(['FAM:CHAN', 'FAM:_UID']);
+        $exclude_facts  = new Collection(['FAM:CHAN', 'FAM:_UID', 'FAM:UID', 'FAM:SUBM']);
         // Don't show tags that are shown in tabs or sidebars
         $exclude_facts = $exclude_facts->merge($sidebar_facts)->merge($tab_facts);
 
